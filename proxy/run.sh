2c49fb5a65da4a5e0e844bd6a4c9eae2bc8a8da5
pismalej <paulo.ismalej@gmail.com>
2024-11-01 00:40:33 -0600

    ssl certificate changes

--- proxy/run.sh
+++ proxy/run.sh
@@ -3,7 +3,4 @@
 set -e
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf#
 
-# For Let's Encrypt - obtain and renew certificates
-certbot certonly --nginx -d appopica.org --non-interactive --agree-tos -m paulo.ismalej@gmail.com
-
 nginx -g 'daemon off;'
